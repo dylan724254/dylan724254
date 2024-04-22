06b5c757a22b4ece31d9217db1e238f5647512ac
Dylan_Chen <dylan_chen@17life.com>
2024-04-22 14:35:01 +0800

    加入github action測試code

---
+++ ProfileEdit/ProfileEdit/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ProfileEdit
+{
+    internal class Program
+    {
+        const string pathPrefix = @"../../../../..";
+        const string profileFileName = "ReadMe.md";
+        static void Main(string[] args)
+        {
+            var text = args.Length > 0
+                ? string.Join(",", args)
+                : $"{DateTime.Now:yyyyMMddHHmmss}";
+
+            var filePath = Path.Combine(pathPrefix, profileFileName);
+
+            File.Delete(filePath);
+            using var writer = File.CreateText(filePath);
+            writer.WriteLine(text);
+        }
+    }
+}
