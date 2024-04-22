d81413129aa1068e26d7868813fe4a817dae49ea
Dylan_Chen <dylan_chen@17life.com>
2024-04-22 21:08:10 +0800

    fix

--- ProfileEdit/ProfileEdit/Program.cs
+++ ProfileEdit/ProfileEdit/Program.cs
@@ -179,12 +179,19 @@ namespace ProfileEdit
         {
             for (int i = 0; i < 3; i++)
             {
-                if ((board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
-                    || (board[0, i] == board[1, i] && board[1, i] == board[2, i]))
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
                 {
                     if (board[i, 0] != 0)
                     {
                         return board[i, 0];
+                    }
+                }
+
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    if (board[0, i] != 0)
+                    {
+                        return board[0, i];
                     }
                 }
             }
