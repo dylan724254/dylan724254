1659672ebed225f76566d92d8ad07dd77ac66291
Dylan_Chen <dylan_chen@17life.com>
2024-04-23 22:41:52 +0800

    poc

--- ProfileEdit/ProfileEdit/Program.cs
+++ ProfileEdit/ProfileEdit/Program.cs
@@ -76,7 +76,7 @@ namespace ProfileEdit
             using var writer = File.CreateText(filePath);
             writer.WriteLine(GetTitle(boardState));
             writer.WriteLine(StateToDisplay(boardState));
-            writer.WriteLine($"<br/>\"最後玩家 <a href=\"https://github.com/{player}\">@{player}</a>");
+            writer.WriteLine($"<br/>最後玩家 <a href=\"https://github.com/{player}\">@{player}</a>");
             writer.WriteLine(GetSelectArea(boardState));
         }
 
